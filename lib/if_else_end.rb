766c17aaf913ea520ba2f74fc4a7ed76f3d53e54
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-10 21:15:36 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -2,17 +2,10 @@
 current_time = Time.now.to_i
 even_seconds = current_time / 2
 
-#if current_time / 2
-#  puts "Even!"
-#end
-
-#if current_time / 3
-#  puts "Odd!"
-#end
-
 if current_time / 2
   puts "Even!"
-elsif current_time / 3
+end
+
+if current_time / 3
   puts "Odd!"
-else 
 end
